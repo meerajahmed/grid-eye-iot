36ad751b834e88cdad4c6f59c59a00402b7259f9
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2019-07-23 16:33:46 +0530

    Emit event to server

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const socketio = require('socket.io-client');
+
+const app = express();
+const port = process.env.PORT || 3001; // port set by heroku
+const io = socketio('http://localhost:3000');
+
+getRandomInt = (max) =>  Math.floor(Math.random() * Math.floor(max));
+
+app.get('/', (req, res) => res.send('Grid Eye IOT'));
+
+io.on('connect', () => {
+  console.log('Grid eye iot connected');
+  setInterval(() => {
+    io.emit('from_grid_eye', {
+      time: Date.now(),
+      count: getRandomInt(10),
+      id: getRandomInt(4)
+    });
+  }, 2000);
+});
+
+io.on('disconnect', () => console.log('Grid eye iot disconnected'));
+
+app.listen(port, () => {
+  console.log(`App is running: 🌎 http://localhost:${port}`)
+});
